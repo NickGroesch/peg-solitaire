2d8ae0b609802ec36ab8391146e0e41f6f25c24a
NickGroesch <nicholasgroesch2019@u.northwestern.edu>
2021-04-06 08:34:47 -0500

    have a big equilateral triangle

---
+++ peg-solitaire/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pegboard from "./components/Pegboard"
+
+function App() {
+  return (
+    <div style={{ height: "100vh" }} id='background'>
+      <Pegboard />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ peg-solitaire/src/components/Pegboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Pegboard() {
+    return (
+        <svg viewBox="0 0 1000 1000">
+            <polygon points="500,0 0,866 1000,866" />
+
+
+        </svg>
+    );
+}
+
+export default Pegboard;
